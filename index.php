80c400b7e1bccd86d16556ec73f634c3b9160bc1
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-11-20 13:35:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $options = array(
 	)
 );
 $context = stream_context_create($options);
-file_get_contents("https://graph.facebook.com/v6.6/me/messages?access_token=$token", false, $context);
+file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
    
 ?>
 
