dd8b0bd641671edc25687bdbf7d8af730bc1745c
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-11-20 13:29:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
+ 
 <?php
-$token = "EAAcShUvBPaIBAHPcQdhwA2wIi4SgNtGZAL4PkUpLHBtkjTmfYkM1bXiKqf1qXcKHHiaus47tBwoBiZCAPePiIZCMGvffhsU8ZAdohzblWWu7dkgYKvYOmmEblZCPeZBOcUYw8C92D6clSKprMa8LnWg9rqU4q2bTZCA4JZC3yv1XLZAiiEIcavbvM";
- 
-file_put_contents("message.txt",file_get_contents("php://input"));
-$fb_message = file_get_contents("message.txt");
-$fb_message = json_decode($fb_message);
-$rec_id = $fb_message->entry[0]->messaging[0]->sender->id; //Sender's ID
-$rec_msg= $fb_message->entry[0]->messaging[0]->message->text; //Sender's Message
-$data_to_send = array(
-'recipient'=> array('id'=>"$rec_id"), //ID to reply
-'message' => array('text'=>"Hi I am Test Bot") //Message to reply
-);
- 
-$options_header = array ( //Necessary Headers
-'http' => array(
-'method' => 'POST',
-'content' => json_encode($data_to_send),
-'header' => "Content-Type: application/json\n"
-)
-);
-$context = stream_context_create($options_header);
-file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token",false,$context);
 
+file_put_contents("fb.tx", file_get_contents("php://input"));    
+
+$fb = fie_get_contents("fb.txt");
+
+$fb = json_decode($fb);
+$rid = $fb->entry[0]->sender-id;
+
+
+$token = "EAAcShUvBPaIBAHPcQdhwA2wIi4SgNtGZAL4PkUpLHBtkjTmfYkM1bXiKqf1qXcKHHiaus47tBwoBiZCAPePiIZCMGvffhsU8ZAdohzblWWu7dkgYKvYOmmEblZCPeZBOcUYw8C92D6clSKprMa8LnWg9rqU4q2bTZCA4JZC3yv1XLZAiiEIcavbvM";
+
+$data = array(
+	'recipient' => array('id'=>"$rid"),
+	'message'   => array('text' => "Nice to meet you!")
+);
+
+$options = array(
+		'http' => array(
+			'method' => 'POST',
+			'content' => json_encode($data),
+			'header' => "Content-Type: appication/jso\n"
+	)
+);
+$context = stream_context_create($options);
+file_get_contents("https://graph.facebook.com/v6.6/me/messages?access_token=$token", false, $context);
+   
 ?>
+
+
+
